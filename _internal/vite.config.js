fb037d031558f2cb357ada4c9fd93c582aa83f82
Jakub Marcinka <jakub.marcinka@tul.cz>
2026-03-03 07:36:04 +0100

    feat: Konfigurace Vite pro vypnutí minifikace a přizpůsobení názvů výstupních souborů.

--- _internal/vite.config.js
+++ _internal/vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
     build: {
         outDir: '../dist',
         emptyOutDir: true,
+        minify: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
     },
     server: {
         port: 5173,
